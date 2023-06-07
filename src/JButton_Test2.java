c1fe32d6ffeeb55638d2658ccf540cc0112bca30
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-06-07 19:24:51 +0900

    JButton과 for문을 이용한 예제 추가

---
+++ src/JButton_Test2.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class JButton_Test2 {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        jf.setSize(300, 300);
+        jf.setTitle("JButton2");
+        jf.setLayout(null);
+
+        String[] name = {"1번", "2번", "3번"};
+        for (int i=0; i<name.length; i++) {
+            JButton jb = new JButton(name[i]);
+            jb.setSize(140, 50);
+            jb.setLocation(20, 30+70*i);
+
+            jf.add(jb);
+        }
+        jf.setVisible(true);
+    }
+
+}

23f0e9e147ba6520981d12c35b487c0295aa2800
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-06-08 09:31:49 +0900

    for문 개선, 배열 선언타입 변경

--- src/Layout/FlowLayout_Test.java
+++ src/Layout/FlowLayout_Test.java
@@ -11,9 +11,9 @@ public class FlowLayout_Test extends JFrame{
         JPanel jp = new JPanel(new FlowLayout(FlowLayout.CENTER));
         //jp라는 패널을 생성하는데 배치관리자를 FlowLayout으로 하고 가운데 정
 
-        String ButtonName[] = {"컴포넌트1", "컴포넌트2", "컴포넌트3", "컴포넌트4", "컴포넌트5", "컴포넌트6", "컴포넌트7", "컴포넌트8", "컴포넌트9"};
-        for(int i=0; i<ButtonName.length; i++) {
-            JButton jb = new JButton(ButtonName[i]);
+        String[] ButtonName = {"컴포넌트1", "컴포넌트2", "컴포넌트3", "컴포넌트4", "컴포넌트5", "컴포넌트6", "컴포넌트7", "컴포넌트8", "컴포넌트9"};
+        for (String s : ButtonName) {
+            JButton jb = new JButton(s);
             jp.add(jb);
         }
         add(jp);
